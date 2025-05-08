55935da7b6e2898c4a4f60a32ef471abf029636a
Aluno <aluno@IPIRANGA.FATEC>
2025-05-08 16:12:59 -0300

    feat(p1): jogador pode morrer

--- JogoMinecraft.java
+++ JogoMinecraft.java
@@ -5,6 +5,7 @@ public class JogoMinecraft {
         var gerador = new Random();
         while(true){
             var oQueFazer = 1 + gerador.nextInt(3);
+            var sofrerDanos = gerador.nextDouble();
             switch (oQueFazer) {
                 case 1:
                     personagem1.minerar();
@@ -15,6 +16,11 @@ public class JogoMinecraft {
                 case 3:
                     personagem1.construir();
                     break;
+            }
+            if(sofrerDanos <= 0.25) personagem1.levarDano();
+            if(!personagem1.estaVivo()){
+                System.out.println("GAME OVER");
+                return;
             }
             System.out.println(personagem1);
             System.out.println("============");
