d629fbee9e73de9600da5f4e32cf26d4b5fa820f
Aluno <aluno@IPIRANGA.FATEC>
2025-06-05 17:20:25 -0300

    feat(p2): cria banco de dados

---
+++ script.sql
@@ -0,0 +1,16 @@
+-- CREATE TABLE tb_personagem(  
+--     cod_personagem SERIAL PRIMARY KEY,
+--     nome VARCHAR(200) NOT NULL,
+--     prob_construir DOUBLE PRECISION NOT NULL,
+--     prob_coletar DOUBLE PRECISION NOT NULL,
+--     prob_minerar DOUBLE PRECISION NOT NULL,
+--     n_vitorias INT NOT NULL,
+--     n_derrotas INT NOT NULL
+-- );
+
+-- SELECT * FROM tb_personagem;
+
+-- INSERT INTO tb_personagem(nome, prob_construir, prob_coletar, prob_minerar, n_vitorias, n_derrotas) 
+-- VALUES
+-- ('Alex', '0' , '0', '0', '0', '0'),
+-- ('Rukia', '0' , '0', '0', '0', '0');
