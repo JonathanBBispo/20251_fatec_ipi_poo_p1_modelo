ad2f03800113ce6fb7abef98491eb8201e0c2b6e
Jonathan Bispo <jokz.bis@gmail.com>
2025-06-07 14:03:50 -0300

    fix(p2): arruma probabilidades dos personagens

--- script.sql
+++ script.sql
@@ -1,16 +1,18 @@
--- CREATE TABLE tb_personagem(  
---     cod_personagem SERIAL PRIMARY KEY,
---     nome VARCHAR(200) NOT NULL,
---     prob_construir DOUBLE PRECISION NOT NULL,
---     prob_coletar DOUBLE PRECISION NOT NULL,
---     prob_minerar DOUBLE PRECISION NOT NULL,
---     n_vitorias INT NOT NULL,
---     n_derrotas INT NOT NULL
--- );
+-- CREATE DATABASE "20251_poo_p2"
 
--- SELECT * FROM tb_personagem;
+-- CREATE TABLE tb_personagem(  
+--      cod_personagem SERIAL PRIMARY KEY,
+--      nome VARCHAR(200) NOT NULL,
+--      prob_construir DOUBLE PRECISION NOT NULL,
+--      prob_coletar DOUBLE PRECISION NOT NULL,
+--      prob_minerar DOUBLE PRECISION NOT NULL,
+--      n_vitorias INT NOT NULL,
+--      n_derrotas INT NOT NULL
+-- )
+
+-- SELECT * FROM tb_personagem
 
 -- INSERT INTO tb_personagem(nome, prob_construir, prob_coletar, prob_minerar, n_vitorias, n_derrotas) 
 -- VALUES
--- ('Alex', '0' , '0', '0', '0', '0'),
--- ('Rukia', '0' , '0', '0', '0', '0');
+-- ('Alex', 0.3 , 0.4, 0.3, 0, 0),
+-- ('Rukia', 0.2 , 0.3, 0.5, 0, 0);
